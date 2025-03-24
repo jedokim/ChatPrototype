7bca26114df54e6872d15cb06ca85606722de95a
Jeremy Kim <jedo@alumni.unc.edu>
2025-03-23 21:31:24 -0400

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Jeremy Kim on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Jeremy Kim on 3/23/25.
+//
+
+import Testing
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
